0ca85fe99efb21d820bebd0349bd0ed64724f331
Dominika Limanówka <47063149+xlimiii@users.noreply.github.com>
2020-06-18 08:46:16 +0200

    working menu navigation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,6 +19,10 @@ namespace CoPoleci
         {
             DragMove();
         }
+        private void PowerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
 
         private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -28,8 +32,33 @@ namespace CoPoleci
             switch (index)
             {
                 case 0:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new SeenMovies());
                     break;
                 case 1:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new Movies());
+                    break;
+                case 2:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new Actors());
+                    break;
+                case 3:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new Directors());
+                    break;
+                case 4:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new Companies());
+                    break;
+                case 5:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new MovieRankings());
+                    break;
+                // trzeba zmienić albo nawigację, albo strony na kontrolki w rekomendatorze
+                case 6:
+                    GridPrincipal.Children.Clear();
+                  //  GridPrincipal.Children.Add(new Start());
                     break;
                 default:
                     break;
