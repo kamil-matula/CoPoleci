1055525f1b621b986f1593369805b727067d35e8
MKamil99 <mkamil1999@gmail.com>
2020-06-20 21:00:39 +0200

    Added Data-Inserter

---
+++ DataInserter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace DataInserter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnectionStringBuilder connStrBuilder = new MySqlConnectionStringBuilder
+            {
+                UserID = "root",
+                Password = "",
+                Server = "localhost",
+                Database = "CoPoleci",
+                Port = 3306
+            };
+
+            string[] lines = File.ReadAllLines(args[0]);
+            int attrNumber = lines[0].Split(",").Length;
+            using (var connection = new MySqlConnection(connStrBuilder.ToString()))
+            {
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    MySqlCommand command = new MySqlCommand($"INSERT INTO {args[1]} VALUES {PrepareString(lines[i], attrNumber)}", connection);
+                    connection.Open();
+                    _ = command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+
+        static string PrepareString(string line, int attrNumber)
+        {
+            string tmp = "(";
+            string[] values = line.Split(",");
+            for (int j = 0; j < attrNumber - 1; j++)
+                tmp += $"'{values[j]}', ";
+            tmp += $"'{values[attrNumber - 1]}')";
+            return tmp;
+        }
+    }
+}
