4ddade7432c6c2db78125d23f21b467224d22fd4
Dominika Limanówka <47063149+xlimiii@users.noreply.github.com>
2020-06-22 14:22:33 +0200

    added searching box
    for now just in SeenMovies

--- Tabs/SeenMovies.xaml.cs
+++ Tabs/SeenMovies.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using CoPoleci.DAL;
+using System;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace CoPoleci
 {
@@ -7,6 +10,20 @@ namespace CoPoleci
         public SeenMovies()
         {
             InitializeComponent();
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(items.ItemsSource);
+            view.Filter = ItemSearcher;
+        }
+        private bool ItemSearcher(object item)
+        {
+            if (String.IsNullOrEmpty(searchingBox.Text))
+                return true;
+            else
+                return ((item as Country).Name.IndexOf(searchingBox.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private void SearchingBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(items.ItemsSource).Refresh();
         }
     }
 }
