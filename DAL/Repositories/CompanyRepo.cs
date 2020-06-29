a3a54827c65b251b46de976b6408717afbbd2d51
MKamil99 <mkamil1999@gmail.com>
2020-06-29 12:09:43 +0200

    Added "GetCompanyFromMovie" function

--- DAL/Repositories/CompanyRepo.cs
+++ DAL/Repositories/CompanyRepo.cs
@@ -23,5 +23,25 @@ namespace CoPoleci.DAL
             catch { }
             return companies;
         }
+
+        public static Company GetCompanyFromMovie(Movie movie)
+        {
+            string company = "";
+            try
+            {
+                using (var connection = DBConnection.Instance.Connection)
+                {
+                    MySqlCommand command = new MySqlCommand($"select nazwa_wytwórni from wyprodukował where id_filmu = '{movie.Id}'", connection);
+                    connection.Open();
+                    var dataReader = command.ExecuteReader();
+                    while (dataReader.Read())
+                        company = dataReader["nazwa_wytwórni"].ToString();
+                    connection.Close();
+                }
+            }
+            catch { }
+ 
+            return QueryManager.Companies.Find(i => i.Name == company);
+        }
     }
 }
