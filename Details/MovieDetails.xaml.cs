8dc011e91c028d6e65fcffcf307c7a64092792d8
Dominika Limanówka <47063149+xlimiii@users.noreply.github.com>
2020-07-01 15:56:01 +0200

    added icons and better design to MovieDetails

--- Details/MovieDetails.xaml.cs
+++ Details/MovieDetails.xaml.cs
@@ -43,11 +43,11 @@ namespace CoPoleci
             movieTitle_TextBlock.Text = clickedmovie.Title;
             year_TextBlock.Text = "Rok produkcji: " + clickedmovie.Year.ToString();
             genre_TextBlock.Text = "Gatunek: " + clickedmovie.Genre;
-            posterImage.Children.Add(new Image { Height = 500, Width = 190, Source = clickedmovie.Poster, VerticalAlignment = VerticalAlignment.Top, HorizontalAlignment = HorizontalAlignment.Left });
+            posterImage.Children.Add(new Image { Height = 510, Width = 194, Source = clickedmovie.Poster, VerticalAlignment = VerticalAlignment.Top, HorizontalAlignment = HorizontalAlignment.Left });
 
-            directorPhoto.Children.Add(new Image { Height = 200, Width = 75, Source = DirectorRepo.GetDirectorsFromMovie(clickedmovie)[0].Photo, HorizontalAlignment=HorizontalAlignment.Left});
+            directorPhoto.Children.Add(new Image { Height = 120, Width = 80, Source = DirectorRepo.GetDirectorsFromMovie(clickedmovie)[0].Photo, HorizontalAlignment=HorizontalAlignment.Center, VerticalAlignment=VerticalAlignment.Center});
             directorName.Text = DirectorRepo.GetDirectorsFromMovie(clickedmovie)[0].Name;
-            companyPhoto.Children.Add(new Image { Height = 200, Width = 75, Source = CompanyRepo.GetCompanyFromMovie(clickedmovie).Photo, HorizontalAlignment = HorizontalAlignment.Left });
+            companyPhoto.Children.Add(new Image { Height=90, Width = 120, Source = CompanyRepo.GetCompanyFromMovie(clickedmovie).Photo, HorizontalAlignment = HorizontalAlignment.Left, VerticalAlignment = VerticalAlignment.Bottom});
             companyName.Text = CompanyRepo.GetCompanyFromMovie(clickedmovie).Name;
 
             ActorListView.ItemsSource = ActorRepo.GetActorsFromMovie(clickedmovie);
@@ -66,13 +66,22 @@ namespace CoPoleci
                 if (window.GetType() == typeof(MainWindow))
                     (window as MainWindow).GridPrincipal.Children.Add(new ActorDetails(clickedactor));
         }
+        private void Director_Clicked(object sender, RoutedEventArgs e)
+        {
+           //
+        }
+        private void Company_Clicked(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
 
         private void LoadIcons() {
            
             Image img = new Image
             {
-                Height = 85,
-                Width = 85,
+                Height = 35,
+                Width = 35,
                 HorizontalAlignment = HorizontalAlignment.Center,
                 VerticalAlignment = VerticalAlignment.Center,
                 Margin = new Thickness(1, 1, 1, 1),
@@ -81,8 +90,8 @@ namespace CoPoleci
 
             Image img2 = new Image
             {
-                Height = 85,
-                Width = 85,
+                Height = 35,
+                Width = 35,
                 HorizontalAlignment = HorizontalAlignment.Center,
                 VerticalAlignment = VerticalAlignment.Center,
                 Margin = new Thickness(1, 1, 1, 1),
@@ -91,28 +100,48 @@ namespace CoPoleci
 
             Image img3 = new Image
             {
-                Height = 85,
-                Width = 85,
+                Height = 105,
+                Width = 105,
                 HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Top,
                 Margin = new Thickness(1, 1, 1, 1),
                 Source = new BitmapImage(new Uri($@"\Graphics\Images\bactors.png", UriKind.Relative))
             };
 
-            //Image img4 = new Image
-            //{
-            //    Height = 85,
-            //    Width = 85,
-            //    HorizontalAlignment = HorizontalAlignment.Center,
-            //    VerticalAlignment = VerticalAlignment.Center,
-            //    Margin = new Thickness(1, 1, 1, 1),
-            //    Source = new BitmapImage(new Uri($@"\Graphics\Images\bseen.png", UriKind.Relative))
-            //};
+            Image img4 = new Image
+            {
+                Height = 65,
+                Width = 65,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(1, 1, 1, 1),
+                Source = new BitmapImage(new Uri($@"\Graphics\Images\bseen.png", UriKind.Relative))
+            };
+            Image img5 = new Image
+            {
+                Height = 65,
+                Width = 65,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(1, 1, 1, 1),
+                Source = new BitmapImage(new Uri($@"\Graphics\Images\bseenyes.png", UriKind.Relative))
+            };
+            Image img6 = new Image
+            {
+                Height = 40,
+                Width = 80,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(1, 1, 1, 1),
+                Source = new BitmapImage(new Uri($@"\Graphics\Images\arrow.png", UriKind.Relative))
+            };
 
             iconDirector.Children.Add(img);
             iconCompany.Children.Add(img2);
             iconActor.Children.Add(img3);
-            //iconSeen.Children.Add(img4);
+            notseenIcon.Children.Add(img4);
+            seenIcon.Children.Add(img5);
+            array.Children.Add(img6);
         }
 
         
